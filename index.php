e713b5fcac410ac14e53514acf7b52a7123d2bc5
HR <mail@hrcc.sk>
2013-09-28 15:44:25 +0200

    Initial commit Ver 1.0

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+
+		<title>Applist @ Raphael</title>
+
+		<link rel="stylesheet" href="assets/css/style.css" type="text/css" media="screen" charset="utf-8" />
+	</head>
+
+	<body>
+		<?php
+			function getApps($path="../") {
+				$apps = scandir($path);
+
+				$appsno = 0;
+
+				foreach ($apps as $app) {
+					if ($app != '.' && $app != '..' && $app != 'list'){
+						$appsno++;
+						echo "<li>
+								<div class=\"content\">
+									<a href=\"/$app\">
+										$app
+										<img src=\"assets/images/arrow.png\"/>
+									</a>
+								</div>
+							  </li>";
+					}
+				}
+
+				if ($appsno == 0) {
+					echo "<li>
+							<div class=\"content\">
+								EMPTY
+							</div>
+						  </li>";
+				}
+			}
+		?>
+
+		<div id="settings-panel">
+			<header>
+				<span class="title"><strong>Application List @ Raphael</strong></span>
+			</header>
+
+			<section id="content">
+				<ul>
+					<?php getApps(); ?>
+				</ul>
+			</section>
+		</div>
+	</body>
+</html>
